bbefc53e60671bfbb0f328d17fe68900811f8c4e
akkiee-daiki <daikiakibayashi@aol.com>
2024-03-31 23:19:07 +0900

    content.js modify indent

--- content.js
+++ content.js
@@ -1,31 +1,31 @@
-window.onload = function(){
-    const bodyTag = document.getElementsByTagName("body")[0];
+window.onload = function () {
+  const bodyTag = document.getElementsByTagName("body")[0];
 
-    // 許可されているYouTube等のURLを修正(playListなど(YouTube APIから取得))
-    let allowedUrlList = [
-        // name: 'URL'
-        {title: "【2分】胸の上部・中部・下部を鍛える大胸筋トレ【プッシュアップバー自重トレ】", url: "https://www.youtube.com/watch?v=ZI7URsS6VrA&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=10"},
-        {title: "【たった1セット】誰でも必ず効く！大胸筋がパンパンになるやばいメニュー", url: "https://www.youtube.com/watch?v=bDz45AJjpEM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=2"},
-        {title: "【筋トレ】本気で腹筋を割りたい人必見！最強腹筋トレーニング", url: "https://www.youtube.com/watch?v=ZEucxLf5x-c&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=3"},
-        {title: "【3分30秒】強靭な体を作り上げる体幹トレーニング8種目!!【インナーマッスル】", url: "https://www.youtube.com/watch?v=Vcb2NrOyMxY&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=4"},
-        {title: "【こりゃやべぇww】腹筋バキバキ!!追い込みまくる地獄の4分間？？", url: "https://www.youtube.com/watch?v=nh7zNUq2MzM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=5"},
-        {title: "【ルーティーン】12分間の大臀筋(お尻)トレーニング！1週間で全身を鍛える！", url: "https://www.youtube.com/watch?v=2p7tHVT4pO8&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=6"},
-        {title: "ダンベルを使った筋トレ｜おしり【公式】", url: "https://www.youtube.com/watch?v=wwIlWyuo9DA&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=7"},
-        {title: "腰の振り方・動かし方のコツ【体の使い方レッスン動画】", url: "https://www.youtube.com/watch?v=j1iDzlHOzJE&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=8"},
-        {title: "女性が喜ぶ腰の振り方【テクニック伝授】", url: "https://www.youtube.com/watch?v=xKovbi3Z2QM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=9"},
-        {title: "【7分】これ一回で十分！超高強度下半身トレーニング【筋トレ】", url: "https://www.youtube.com/watch?v=LOuh44mpQRg&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=11"},
-        {title: "【静かにできるHIIT】スクワットだけ!!脂肪がガンガン燃える自宅トレ!!", url: "https://www.youtube.com/watch?v=iWlpOVUTjag&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=12"},
-        {title: "肩全体に効きまくる！7分間ダンベルトレーニング", url: "https://www.youtube.com/watch?v=wpKUHfReW-0&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=13"},
-        {title: "３分間で肩全体に効く！ダンベルだけで６種目の肩トレーニングをご一緒に【初心者向け】解説なし", url: "https://www.youtube.com/watch?v=f6CeYdgX02U&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=14"},
-        {title: "肩の筋肉だけにめっちゃ効く！3分6種目のダンベルトレーニング", url: "https://www.youtube.com/watch?v=gty-g-9lwJY&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=15"},
-        {title: "自宅で最強背中トレ！ダンベルだけで背中全体に効く３分間メニュー！広背筋、僧帽筋、大円筋全てに効きます。", url: "https://www.youtube.com/watch?v=_qZccBRFm6I&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=16"},
-        {title: "【めちゃ効く】最速でシックスパックを作る腹筋トレーニング6分間！", url: "https://www.youtube.com/watch?v=yfF-Q2NhVnE&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=17"},
-        {title: "【最強背中トレ】ダンベルだけで背中の全てが鍛えられる5種目のトレーニング！解説Ver.", url: "https://www.youtube.com/watch?v=_wkW3kqzyvU&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=18"},
-        {title: "【4分】腹回りの脂肪を燃焼させる腹筋サーキット【腰と横腹も鍛える７種目】", url: "https://www.youtube.com/watch?v=ihsZ5mylHk0&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=19"},
-        {title: "初心者が背中に筋肉を付けるためのベストメニュー【ダンベルだけ】", url: "https://www.youtube.com/watch?v=6M7MyNE5gZo&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=20"},
-    ];
+  // 許可されているYouTube等のURLを修正(playListなど(YouTube APIから取得))
+  let allowedUrlList = [
+    // name: 'URL'
+    {title: "【2分】胸の上部・中部・下部を鍛える大胸筋トレ【プッシュアップバー自重トレ】", url: "https://www.youtube.com/watch?v=ZI7URsS6VrA&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=10"},
+    {title: "【たった1セット】誰でも必ず効く！大胸筋がパンパンになるやばいメニュー", url: "https://www.youtube.com/watch?v=bDz45AJjpEM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=2"},
+    {title: "【筋トレ】本気で腹筋を割りたい人必見！最強腹筋トレーニング", url: "https://www.youtube.com/watch?v=ZEucxLf5x-c&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=3"}, 
+    {title: "【3分30秒】強靭な体を作り上げる体幹トレーニング8種目!!【インナーマッスル】", url: "https://www.youtube.com/watch?v=Vcb2NrOyMxY&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=4"},
+    {title: "【こりゃやべぇww】腹筋バキバキ!!追い込みまくる地獄の4分間？？", url: "https://www.youtube.com/watch?v=nh7zNUq2MzM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=5"}, 
+    {title: "【ルーティーン】12分間の大臀筋(お尻)トレーニング！1週間で全身を鍛える！", url: "https://www.youtube.com/watch?v=2p7tHVT4pO8&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=6"},
+    {title: "ダンベルを使った筋トレ｜おしり【公式】", url: "https://www.youtube.com/watch?v=wwIlWyuo9DA&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=7"},
+    {title: "腰の振り方・動かし方のコツ【体の使い方レッスン動画】", url: "https://www.youtube.com/watch?v=j1iDzlHOzJE&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=8"},
+    {title: "女性が喜ぶ腰の振り方【テクニック伝授】", url: "https://www.youtube.com/watch?v=xKovbi3Z2QM&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=9"},
+    {title: "【7分】これ一回で十分！超高強度下半身トレーニング【筋トレ】", url: "https://www.youtube.com/watch?v=LOuh44mpQRg&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=11"},
+    {title: "【静かにできるHIIT】スクワットだけ!!脂肪がガンガン燃える自宅トレ!!", url: "https://www.youtube.com/watch?v=iWlpOVUTjag&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=12"},
+    {title: "肩全体に効きまくる！7分間ダンベルトレーニング", url: "https://www.youtube.com/watch?v=wpKUHfReW-0&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=13"},
+    {title: "３分間で肩全体に効く！ダンベルだけで６種目の肩トレーニングをご一緒に【初心者向け】解説なし", url: "https://www.youtube.com/watch?v=f6CeYdgX02U&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=14"},
+    {title: "肩の筋肉だけにめっちゃ効く！3分6種目のダンベルトレーニング", url: "https://www.youtube.com/watch?v=gty-g-9lwJY&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=15"},
+    {title: "自宅で最強背中トレ！ダンベルだけで背中全体に効く３分間メニュー！広背筋、僧帽筋、大円筋全てに効きます。", url: "https://www.youtube.com/watch?v=_qZccBRFm6I&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=16"},
+    {title: "【めちゃ効く】最速でシックスパックを作る腹筋トレーニング6分間！", url: "https://www.youtube.com/watch?v=yfF-Q2NhVnE&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=17"},
+    {title: "【最強背中トレ】ダンベルだけで背中の全てが鍛えられる5種目のトレーニング！解説Ver.", url: "https://www.youtube.com/watch?v=_wkW3kqzyvU&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=18"},
+    {title: "【4分】腹回りの脂肪を燃焼させる腹筋サーキット【腰と横腹も鍛える７種目】", url: "https://www.youtube.com/watch?v=ihsZ5mylHk0&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=19"},
+    {title: "初心者が背中に筋肉を付けるためのベストメニュー【ダンベルだけ】", url: "https://www.youtube.com/watch?v=6M7MyNE5gZo&list=PLJqbpYzIaQTRdfs4wgRPFAD48pGljciTH&index=20"}
+  ];
 
-    bodyTag.innerHTML += `
+  bodyTag.innerHTML += `
     <style>
     .modal-container.active {
         opacity: 1;
@@ -173,16 +173,12 @@ window.onload = function(){
         <script src="content.js" defer></script>
     `;
 
-    // modalが表示されている場合のみに処理を限定
-    const netDefModal = document.getElementById("js-netDefModal");
-    const netDefModalCloseBtn = document.getElementById("js-netDefModalClose");
-    if (!netDefModal || !netDefModalCloseBtn) {
-        netDefModalCloseBtn.addEventListener("click", () => {
-            netDefModal.style.display = "none";
-        });
-    }
-    
-}
-
-
-
+  // modalが表示されている場合のみに処理を限定
+  const netDefModal = document.getElementById("js-netDefModal");
+  const netDefModalCloseBtn = document.getElementById("js-netDefModalClose");
+  if (!netDefModal || !netDefModalCloseBtn) {
+    netDefModalCloseBtn.addEventListener("click", () => {
+      netDefModal.style.display = "none";
+    });
+  }
+};
